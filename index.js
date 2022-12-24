53b7788dd62a461e867d6728be6be29627db83e1
sarahrahman25 <121226243+sarahrahman25@users.noreply.github.com>
2022-12-24 21:02:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var menubtn = document.getElementById("menubtn");
+var sidenav = document.getElementById("sidenav");
+sidenav.style.right= "-360px";
+menubtn.onclick=function(){
+    if(sidenav.style.right== "-360px"){
+    sidenav.style.right= "0";
+    }
+    else{
+        sidenav.style.right= "-360px";
+    }
+}
+document.querySelectorAll(buttom)
+
